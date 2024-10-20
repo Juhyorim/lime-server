061e7462940e88245f42e231fd56539b6d6afa77
Juhyorim <joohr1234@gmail.com>
2024-10-20 19:20:38 +0900

    initial commit!

---
+++ src/main/java/com/lime/limeserver/LimeServerApplication.java
@@ -0,0 +1,13 @@
+package com.lime.limeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimeServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimeServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lime/limeserver/LimeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lime.limeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimeServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
