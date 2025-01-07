a4493ade25342401d703d1aef3a07a9184db2aac
Juhyorim <joohr1234@gmail.com>
2025-01-07 19:18:07 +0900

    test: 버스 정류장 API 테스트 작성

--- server/src/test/java/com/lime/server/api/BusStopApiTest.java
+++ server/src/test/java/com/lime/server/api/BusStopApiTest.java
@@ -2,8 +2,10 @@ package com.lime.server.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lime.server.BusApiTest;
-import com.lime.server.bus.dto.cityApi.CityCodeApiResponse;
-import com.lime.server.bus.dto.cityApi.CityCodeDto;
+import com.lime.server.bus.dto.cityApi.BusStopApiResponse;
+import com.lime.server.bus.dto.cityApi.BusStopApiResponse.BusStop;
+import com.lime.server.bus.dto.cityApi.CityApiResponse;
+import com.lime.server.bus.dto.cityApi.CityApiResponse.City;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -23,17 +25,18 @@ public class BusStopApiTest {
     public String serviceKey;
 
     static String CITY_CODE_API_URL = "http://apis.data.go.kr/1613000/BusSttnInfoInqireService/getCtyCodeList";
+    static String BUS_STOP_API_URL = "http://apis.data.go.kr/1613000/BusSttnInfoInqireService/getSttnNoList";
 
     static final Logger logger = LoggerFactory.getLogger(BusApiTest.class);
 
     @Test
-    void cityCodeApi() throws IOException {
+    void cityCodeApiTest() throws IOException {
         //City Code: 37050, City Name: 구미시
-
         StringBuilder urlBuilder = new StringBuilder(CITY_CODE_API_URL);
         urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + "=" + serviceKey);
         urlBuilder.append(
                 "&" + URLEncoder.encode("_type", "UTF-8") + "=" + URLEncoder.encode("json", "UTF-8"));
+
         URL url = new URL(urlBuilder.toString());
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
@@ -57,12 +60,59 @@ public class BusStopApiTest {
         conn.disconnect();
 
         ObjectMapper objectMapper = new ObjectMapper();
-        CityCodeApiResponse cityCodeApiResponse = objectMapper.readValue(sb.toString(), CityCodeApiResponse.class);
+        CityApiResponse cityCodeApiResponse = objectMapper.readValue(sb.toString(), CityApiResponse.class);
 
         if ("00".equals(cityCodeApiResponse.getResponse().getHeader().getResultCode())) {
-            List<CityCodeDto> cities = cityCodeApiResponse.getResponse().getBody().getItems().getItem();
-            for (CityCodeDto city : cities) {
+            CityApiResponse.Items cityApiItems = cityCodeApiResponse.getResponse().getBody().getItems();
+            List<City> cities = cityApiItems.getItem();
+            for (City city : cities) {
                 logger.info(() -> "City Code: " + city.getCitycode() + ", City Name: " + city.getCityname());
+            }
+        }
+    }
+
+    @Test
+    void busStopApiTest() throws IOException {
+        StringBuilder urlBuilder = new StringBuilder(BUS_STOP_API_URL);
+        urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + "=" + serviceKey);
+        urlBuilder.append(
+                "&" + URLEncoder.encode("cityCode", "UTF-8") + "=" + URLEncoder.encode("37050", "UTF-8"));
+        urlBuilder.append(
+                "&" + URLEncoder.encode("_type", "UTF-8") + "=" + URLEncoder.encode("json", "UTF-8"));
+
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        int responseCode = conn.getResponseCode();
+        logger.info(() -> "Response code: " + responseCode);
+
+        BufferedReader rd;
+        if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+        StringBuilder sb = new StringBuilder();
+        String line;
+
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
+        }
+        rd.close();
+        conn.disconnect();
+
+//        logger.info(() -> sb.toString());
+
+        //매핑 제대로 다시
+        ObjectMapper objectMapper = new ObjectMapper();
+        BusStopApiResponse cityCodeApiResponse = objectMapper.readValue(sb.toString(), BusStopApiResponse.class);
+
+        if ("00".equals(cityCodeApiResponse.getResponse().getHeader().getResultCode())) {
+            BusStopApiResponse.Items items = (BusStopApiResponse.Items) cityCodeApiResponse.getResponse().getBody().getItems();
+            List<BusStop> cities = items.getItem();
+            for (BusStop busStop : cities) {
+                logger.info(() -> "node id: " + busStop.getNodeid() + ", nodenm: " + busStop.getNodenm() + ", nodeno: " + busStop.getNodeno());
             }
         }
     }
