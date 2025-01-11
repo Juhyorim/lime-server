6a0c122b017141df5941f18c4abcc1b38db3467a
Juhyorim <joohr1234@gmail.com>
2025-01-11 19:44:44 +0900

    feat: API 에러 코드 상수 정의
    related to: #3

---
+++ server/src/main/java/com/lime/server/busApi/error/APIErrorCode.java
@@ -0,0 +1,21 @@
+package com.lime.server.busApi.error;
+
+public enum APIErrorCode {
+    APPLICATION_ERROR(1, "어플리케이션 에러"),
+    HTTP_ERROR(4, "HTTP 에러"),
+    NO_OPENAPI_SERVICE_ERROR(12, "해당 오픈 API 서비스가 없거나 폐기됨"),
+    SERVICE_ACCESS_DENIED_ERROR(20, "서비스 접근 거부"),
+    LIMITED_NUMBER_OF_SERVICE_REQUESTS_EXCEEDS_ERROR(22, "서비스 요청제한횟수 초과 에러"),
+    SERVICE_KEY_IS_NOT_REGISTERED_ERROR(30, "등록되지 않은 서비스키"),
+    DEADLINE_HAS_EXPIRED_ERROR(31, "사용기간 만료"),
+    UNREGISTERED_IP_ERROR(32, "등록되지 않은 IP"),
+    UNKNOWN_ERROR(99, "기타 에러");
+
+    final int code;
+    final String description;
+
+    APIErrorCode(int code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+}
