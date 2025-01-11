23e9b54ccaddf569b09b03b84c5f812df41439ef
Juhyorim <joohr1234@gmail.com>
2025-01-11 21:25:24 +0900

    feat: 구독, 회원 관련 엔티티 생성

---
+++ server/src/main/java/com/lime/server/tico/entity/Member.java
@@ -0,0 +1,29 @@
+package com.lime.server.tico.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+
+    private String username;
+    private String password;
+    private String nickname;
+    private String email;
+
+    @OneToMany(mappedBy = "member")
+    List<Subscription> subscriptions;
+}
