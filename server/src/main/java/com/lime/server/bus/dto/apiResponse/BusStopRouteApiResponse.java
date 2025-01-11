c67161469b238df6392d0513c4e5e0c40b328259
Juhyorim <joohr1234@gmail.com>
2025-01-11 19:01:38 +0900

    test: 버스정류장에 들리는 버스 노선(버스 번호) 조회 API 테스트 작성

---
+++ server/src/main/java/com/lime/server/bus/dto/apiResponse/BusStopRouteApiResponse.java
@@ -0,0 +1,57 @@
+package com.lime.server.bus.dto.apiResponse;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BusStopRouteApiResponse {
+    private Response response;
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Response {
+        private Header header;
+        private Body body;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Header {
+        private String resultCode;
+        private String resultMsg;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Body {
+        private Items items;
+        private int numOfRows;
+        private int pageNo;
+        private int totalCount;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Items {
+        private List<BusRoute> item;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class BusRoute {
+        private String routeid;
+        private String routeno;
+        private String routetp;
+        private String endnodenm;
+        private String startnodenm;
+    }
+}
--- server/src/test/java/com/lime/server/api/BusStopApiTest.java
+++ server/src/test/java/com/lime/server/api/BusStopApiTest.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lime.server.BusApiTest;
 import com.lime.server.bus.dto.apiResponse.BusStopApiResponse;
 import com.lime.server.bus.dto.apiResponse.BusStopApiResponse.BusStop;
+import com.lime.server.bus.dto.apiResponse.BusStopRouteApiResponse;
+import com.lime.server.bus.dto.apiResponse.BusStopRouteApiResponse.BusRoute;
 import com.lime.server.bus.dto.apiResponse.CityApiResponse;
 import com.lime.server.bus.dto.apiResponse.CityApiResponse.City;
 import java.io.BufferedReader;
@@ -26,6 +28,7 @@ public class BusStopApiTest {
 
     static String CITY_CODE_API_URL = "http://apis.data.go.kr/1613000/BusSttnInfoInqireService/getCtyCodeList";
     static String BUS_STOP_API_URL = "http://apis.data.go.kr/1613000/BusSttnInfoInqireService/getSttnNoList";
+    static String BUS_STOP_ROUTE_API_URL = "http://apis.data.go.kr/1613000/BusSttnInfoInqireService/getSttnThrghRouteList";
 
     static final Logger logger = LoggerFactory.getLogger(BusApiTest.class);
 
@@ -113,6 +116,62 @@ public class BusStopApiTest {
             List<BusStop> cities = items.getItem();
             for (BusStop busStop : cities) {
                 logger.info(() -> "node id: " + busStop.getNodeid() + ", nodenm: " + busStop.getNodenm() + ", nodeno: " + busStop.getNodeno());
+            }
+        }
+    }
+
+    @Test
+    void busStopRouteApiTest() throws IOException {
+        StringBuilder urlBuilder = new StringBuilder(BUS_STOP_ROUTE_API_URL);
+        urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + "=" + serviceKey);
+        urlBuilder.append(
+                "&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("100", "UTF-8"));
+        urlBuilder.append(
+                "&" + URLEncoder.encode("cityCode", "UTF-8") + "=" + URLEncoder.encode("37050", "UTF-8"));
+        urlBuilder.append(
+                "&" + URLEncoder.encode("_type", "UTF-8") + "=" + URLEncoder.encode("json", "UTF-8"));
+        urlBuilder.append(
+                "&" + URLEncoder.encode("nodeId", "UTF-8") + "=" + URLEncoder.encode("GMB708", "UTF-8"));
+
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        int responseCode = conn.getResponseCode();
+        logger.info(() -> "Response code: " + responseCode);
+
+        BufferedReader rd;
+        if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+        StringBuilder sb = new StringBuilder();
+        String line;
+
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
+        }
+        rd.close();
+        conn.disconnect();
+
+//        logger.info(() -> sb.toString());
+
+        //매핑 제대로 다시
+        ObjectMapper objectMapper = new ObjectMapper();
+        BusStopRouteApiResponse cityCodeApiResponse = objectMapper.readValue(sb.toString(), BusStopRouteApiResponse.class);
+
+        if ("00".equals(cityCodeApiResponse.getResponse().getHeader().getResultCode())) {
+            BusStopRouteApiResponse.Items items = (BusStopRouteApiResponse.Items) cityCodeApiResponse.getResponse().getBody().getItems();
+            List<BusRoute> busRoutes = items.getItem();
+            for (BusRoute route : busRoutes) {
+                logger.info(() ->
+                        "routeid id: " + route.getRouteid()
+                                + ", routeno: " + route.getRouteno()
+                                + ", routetp: " + route.getRoutetp()
+                                + ", endnodenm: " + route.getEndnodenm()
+                                + ", startnodenm: " + route.getStartnodenm()
+                );
             }
         }
     }
