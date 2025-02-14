38829f64215ff6f174ca4c6c41457198bbe18e4d
Juhyorim <joohr1234@gmail.com>
2025-02-15 00:36:40 +0900

    feat: 버스 도착 정보 API Service 로직 생성

--- server/src/main/java/com/lime/server/busApi/service/BusApiService.java
+++ server/src/main/java/com/lime/server/busApi/service/BusApiService.java
@@ -1,6 +1,9 @@
 package com.lime.server.busApi.service;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lime.server.busApi.dto.apiResponse.BusArriveApiResponse;
+import com.lime.server.busApi.dto.apiResponse.BusArriveApiResponse.ArriveBus;
 import com.lime.server.busApi.dto.apiResponse.BusStopApiResponse;
 import com.lime.server.busApi.dto.apiResponse.BusStopRouteApiResponse;
 import com.lime.server.busApi.dto.apiResponse.CityApiResponse;
@@ -25,6 +28,7 @@ public class BusApiService {
     private static final String CITY_CODE_API_URL = "http://apis.data.go.kr/1613000/BusSttnInfoInqireService/getCtyCodeList";
     private static final String BUS_STOP_API_URL_FORMAT = "http://apis.data.go.kr/1613000/BusSttnInfoInqireService/getSttnNoList?serviceKey=%s&cityCode=%s&_type=json&numOfRows=50&pageNo=%d";
     private static final String BUS_STOP_ROUTE_API_URL = "http://apis.data.go.kr/1613000/BusSttnInfoInqireService/getSttnThrghRouteList";
+    private static final String ARRIVE_BUS_API_URL = "http://apis.data.go.kr/1613000/ArvlInfoInqireService/getSttnAcctoArvlPrearngeInfoList";
 
     @Value("${api.serviceKey}")
     public String serviceKey;
@@ -145,5 +149,65 @@ public class BusApiService {
 //        }
 
         return BusRouteResponse.from(busStopRouteApiResponse);
+    }
+
+    public void getArriveBuses() throws IOException {
+        StringBuilder urlBuilder = new StringBuilder(ARRIVE_BUS_API_URL);
+        urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + "=" + serviceKey);
+        urlBuilder.append(
+                "&" + URLEncoder.encode("_type", "UTF-8") + "=" + URLEncoder.encode("json", "UTF-8"));
+        urlBuilder.append(
+                "&" + URLEncoder.encode("cityCode", "UTF-8") + "=" + URLEncoder.encode("37050", "UTF-8"));
+        urlBuilder.append(
+                "&" + URLEncoder.encode("nodeId", "UTF-8") + "=" + URLEncoder.encode("GMB708", "UTF-8"));
+
+        URL url = new URL(urlBuilder.toString());
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        int responseCode = conn.getResponseCode();
+        log.info("Response code: " + responseCode);
+
+        BufferedReader rd;
+        if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+        StringBuilder sb = new StringBuilder();
+        String line;
+
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
+        }
+        rd.close();
+        conn.disconnect();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true); //빈 문자열이 올 때(도착 예정 버스가 없을 때) 처리
+        BusArriveApiResponse cityCodeApiResponse = objectMapper.readValue(sb.toString(), BusArriveApiResponse.class);
+
+        if ("00".equals(cityCodeApiResponse.getResponse().getHeader().getResultCode())) {
+            BusArriveApiResponse.Items busArriveApiItems = cityCodeApiResponse.getResponse().getBody().getItems();
+
+            if (busArriveApiItems == null) {
+                return;
+            }
+
+            List<ArriveBus> cities = busArriveApiItems.getItem();
+            for (ArriveBus bus : cities) {
+                log.info(
+                        "arrprevstationcnt: " + bus.getArrprevstationcnt()
+                                + ", arrtime: " + bus.getArrtime()
+                                + ", nodeid: " + bus.getNodeid()
+                                + ", nodenm: " + bus.getNodenm()
+                                + ", routeid: " + bus.getRouteid()
+                                + ", routeno: " + bus.getRouteno() //버스번호
+                                + ", routetp: " + bus.getRoutetp()
+                                + ", vehicletp: " + bus.getVehicletp()
+                );
+            }
+        }
     }
 }
--- server/src/test/java/com/lime/server/api/BusArriveApiTest.java
+++ server/src/test/java/com/lime/server/api/BusArriveApiTest.java
@@ -1,88 +1,23 @@
 package com.lime.server.api;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lime.server.BusApiTest;
-import com.lime.server.busApi.dto.apiResponse.BusArriveApiResponse;
-import com.lime.server.busApi.dto.apiResponse.BusArriveApiResponse.ArriveBus;
-import java.io.BufferedReader;
+import com.lime.server.busApi.service.BusApiService;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.logging.Logger;
 import org.junit.platform.commons.logging.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class BusArriveApiTest {
-    @Value("${api.serviceKey}")
-    public String serviceKey;
-
-    static String CITY_CODE_API_URL = "http://apis.data.go.kr/1613000/ArvlInfoInqireService/getSttnAcctoArvlPrearngeInfoList";
     static final Logger logger = LoggerFactory.getLogger(BusApiTest.class);
+
+    @Autowired
+    private BusApiService busApiService;
 
     @Test
     void arriveApiTest() throws IOException {
-        StringBuilder urlBuilder = new StringBuilder(CITY_CODE_API_URL);
-        urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + "=" + serviceKey);
-        urlBuilder.append(
-                "&" + URLEncoder.encode("_type", "UTF-8") + "=" + URLEncoder.encode("json", "UTF-8"));
-        urlBuilder.append(
-                "&" + URLEncoder.encode("cityCode", "UTF-8") + "=" + URLEncoder.encode("37050", "UTF-8"));
-        urlBuilder.append(
-                "&" + URLEncoder.encode("nodeId", "UTF-8") + "=" + URLEncoder.encode("GMB708", "UTF-8"));
-
-        URL url = new URL(urlBuilder.toString());
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        conn.setRequestProperty("Content-type", "application/json");
-        int responseCode = conn.getResponseCode();
-        logger.info(() -> "Response code: " + responseCode);
-
-        BufferedReader rd;
-        if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        } else {
-            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-        }
-        StringBuilder sb = new StringBuilder();
-        String line;
-
-        while ((line = rd.readLine()) != null) {
-            sb.append(line);
-        }
-        rd.close();
-        conn.disconnect();
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true); //빈 문자열이 올 때(도착 예정 버스가 없을 때) 처리
-        BusArriveApiResponse cityCodeApiResponse = objectMapper.readValue(sb.toString(), BusArriveApiResponse.class);
-
-        if ("00".equals(cityCodeApiResponse.getResponse().getHeader().getResultCode())) {
-            BusArriveApiResponse.Items busArriveApiItems = cityCodeApiResponse.getResponse().getBody().getItems();
-
-            if (busArriveApiItems == null) {
-                return;
-            }
-
-            List<ArriveBus> cities = busArriveApiItems.getItem();
-            for (ArriveBus bus : cities) {
-                logger.info(() ->
-                        "arrprevstationcnt: " + bus.getArrprevstationcnt()
-                                + ", arrtime: " + bus.getArrtime()
-                                + ", nodeid: " + bus.getNodeid()
-                                + ", nodenm: " + bus.getNodenm()
-                                + ", routeid: " + bus.getRouteid()
-                                + ", routeno: " + bus.getRouteno() //버스번호
-                                + ", routetp: " + bus.getRoutetp()
-                                + ", vehicletp: " + bus.getVehicletp()
-                );
-            }
-        }
+        busApiService.getArriveBuses();
     }
 }
