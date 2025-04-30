7203a8ea2692b0bfbb6eb45436b4051f237fe833
Juhyorim <joohr1234@gmail.com>
2025-04-30 23:54:10 +0900

    test: member 추가 시 패스워드 인코딩이 잘 동작하는지 확인하는 테스트코드 작성

--- server/src/test/java/com/lime/server/auth/MemberServiceTest.java
+++ server/src/test/java/com/lime/server/auth/MemberServiceTest.java
@@ -1,19 +1,32 @@
-//package com.lime.server.auth;
-//
-//import com.lime.server.auth.service.MemberService;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//@Transactional
-//@SpringBootTest
-//public class MemberServiceTest {
-//    @Autowired
-//    MemberService memberService;
-//
-//    @Test
-//    void createMember() {
-//        memberService.createMember("string", "string", "string", "string");
-//    }
-//}
+package com.lime.server.auth;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.lime.server.auth.service.JwtService;
+import com.lime.server.auth.service.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@SpringBootTest
+public class MemberServiceTest {
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Test
+    void createMember() {
+        Member member = memberService.createMember("username", "password", "nickname", "email");
+
+        assertEquals("username", member.getUsername());
+        assertEquals("nickname", member.getNickname());
+        assertEquals("email", member.getEmail());
+        assertTrue(passwordEncoder.matches("password", member.getPassword()));
+    }
+}
