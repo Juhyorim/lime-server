55e38d69fb00dd27ddf4c0b75a2ffd4cfb00f12e
Juhyorim <joohr1234@gmail.com>
2025-05-27 22:36:58 +0900

    test: 정류소에 도착하는 버스 목록 조회 API 테스트 추가
    - 예외가 발생하지 않고, 빈 배열이 반환되어야함

---
+++ server/src/test/java/com/lime/server/busApi/service/BusApiServiceTest.java
@@ -0,0 +1,43 @@
+package com.lime.server.busApi.service;
+
+import com.lime.server.tico.dto.response.BusRouteResponse;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class BusApiServiceTest {
+    @InjectMocks
+    private BusApiService busApiService;
+
+    @Mock
+    private BusApiClient busApiClient;
+
+    @Test
+    @DisplayName("nodeID가 잘못된 예시")
+    void UnknownNodeIdTest() throws URISyntaxException, IOException {
+        //given
+        String jsonContent = Files.readString(
+                Paths.get(getClass().getResource("/busRouteInfo/unknownNodeId.json").toURI())
+        );
+
+        Mockito.when(busApiClient.getBusRouteInfoResponse(any(), any())).thenReturn(new StringBuilder(jsonContent));
+
+        //when
+        BusRouteResponse busRouteInfo = busApiService.getBusRouteInfo("cityCode", "nodeID");
+
+        //then
+        assertEquals(busRouteInfo.getBusStations().size(), 0);
+    }
+}
