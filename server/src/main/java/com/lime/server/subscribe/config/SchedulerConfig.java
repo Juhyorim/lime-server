2ba92b214ca1da9598ae24fa8a271ddc6d318a06
Juhyorim <joohr1234@gmail.com>
2025-05-27 18:44:10 +0900

    feat: 버스가 안다니는 시간에는 API 호출 안하도록 제한

--- server/src/main/java/com/lime/server/subscribe/config/SchedulerConfig.java
+++ server/src/main/java/com/lime/server/subscribe/config/SchedulerConfig.java
@@ -2,6 +2,7 @@ package com.lime.server.subscribe.config;
 
 import com.lime.server.subscribe.service.SubscribeService;
 import java.io.IOException;
+import java.time.LocalTime;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -13,8 +14,21 @@ import org.springframework.stereotype.Component;
 public class SchedulerConfig {
     private final SubscribeService subscribeService;
 
-    @Scheduled(fixedDelay = 30000)
+    @Scheduled(fixedDelay = 90000)
     public void run() throws IOException {
+        if (isNightTime()) {
+            return; // 밤 11시 이후이거나 아침 6시 이전인 경우 API 호출 x
+        }
+
         subscribeService.getSubscribedBusInfo();
+    }
+
+    private boolean isNightTime() {
+        LocalTime currentTime = LocalTime.now();
+        log.info("테스트용 현재시간: " + currentTime);
+        LocalTime nightStart = LocalTime.of(23, 0); // 밤 11시
+        LocalTime morningEnd = LocalTime.of(6, 0);  // 아침 6시
+
+        return currentTime.isAfter(nightStart) || currentTime.isBefore(morningEnd);
     }
 }
