e360fe16260b354c257123677328445c6b6c429c
Juhyorim <joohr1234@gmail.com>
2025-06-01 21:49:19 +0900

    feat: 서버시간 UTC로 고정
    related to: #9

--- server/src/main/java/com/lime/server/ServerApplication.java
+++ server/src/main/java/com/lime/server/ServerApplication.java
@@ -1,5 +1,7 @@
 package com.lime.server;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -7,9 +9,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @SpringBootApplication
 public class ServerApplication {
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
     }
-
 }
